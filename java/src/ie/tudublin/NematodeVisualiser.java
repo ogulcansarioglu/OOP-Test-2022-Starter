100e5b2032f34ce3b95a6107bf04349e38379a57
ogulcansarioglu <ogulcansarioglu1@gmail.com>
2022-04-27 16:20:41 +0100

    created loadNematodes class that populates nematodes ArrayList

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -9,11 +9,14 @@ import processing.data.TableRow;
 public class NematodeVisualiser extends PApplet
 {
 
+	ArrayList<Nematode> nematodes = new ArrayList<Nematode>();
+
+
 	public void keyPressed()
 	{		
 		if (keyCode == LEFT)
 		{
-			
+
 		}		
 	}
 
@@ -33,6 +36,12 @@ public class NematodeVisualiser extends PApplet
 
 	public void loadNematodes()
 	{
+		Table table = loadTable("nematodes.csv", "header");
+		for(TableRow r: table.rows()) 
+		{
+			Nematode nm = new Nematode(r);
+			nematodes.add(nm);
+		}
 	}
 
 
