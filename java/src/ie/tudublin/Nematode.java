f1fbeef6b131c79c8eb96db27e433a80b9059a04
ogulcansarioglu <ogulcansarioglu1@gmail.com>
2022-04-27 16:10:24 +0100

    generated necessary fields and setters and getters

---
+++ java/src/ie/tudublin/Nematode.java
@@ -0,0 +1,43 @@
+package ie.tudublin;
+
+public class Nematode {
+
+    private String name;
+    private int length;
+    private boolean limbs;
+    private String gender;
+    private boolean eyes;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public boolean isLimbs() {
+        return limbs;
+    }
+    public void setLimbs(boolean limbs) {
+        this.limbs = limbs;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public boolean isEyes() {
+        return eyes;
+    }
+    public void setEyes(boolean eyes) {
+        this.eyes = eyes;
+    }
+
+    
+}
